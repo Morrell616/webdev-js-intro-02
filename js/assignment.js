b3124120ce1aaf1e6f12051f2a679e8c39ea4560
Jen Morrell <morrell616@hotmail.com>
2024-12-17 16:13:22 -0500

    added new variables and functions

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,35 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
-
+const year = "2024";
+const resolution = "Learn more javascript!";
+let willMeetResolution = "true"
 function updateYear() {
+    yearElement.innerText = year;
     // Update this function
 
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
 
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+willMeetResolutionElement.innerText =willMeetResolution;
 }
 
 function render() {
     // Update this function
+    updateResolution();
+    updateWillMeetResolution();
+    updateYear();
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+    render();
 })
--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@
 // that are not declared
 "use strict"
 
+const myName = "Jen";
+console.log (myName)
+console.log (typeof myName)
+
+const numberOne = 21;
+console.log(numberOne)
+console.log (typeof numberOne)
+
+let jobTitle = true;
+console.log (jobTitle)
+console.log (typeof jobTitle)
+
